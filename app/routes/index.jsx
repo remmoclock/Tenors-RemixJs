90c70dab062cb7eff9865d03c0474ece3f421ef8
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-11-03 00:27:03 +0100

    Homepage

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,34 @@
 export default function Index() {
   return (
-    <h1 className="text-3xl text-orange-900 font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-full h-screen p-10 text-gray-400 bg-black font-inter">
+      <div className="container px-4 mx-auto">
+        <div>
+          <div id="title" className="my-10 text-center">
+            <h1 className="text-4xl font-bold text-white">Musique 🎶</h1>
+            <p className="text-xl text-gray-500 text-light">
+              Artistes et concerts
+            </p>
+          </div>
+          <div id="choose" className="p-6 m-10 text-center">
+            <a
+              href="/"
+              className="p-4 text-xl font-medium transition duration-200 ease-in-out bg-gray-900 rounded-xl hover:shadow-lg hover:bg-indigo-600 hover:text-white"
+            >
+              Ajouter un musicien
+            </a>
+          </div>
+          <div className="flex justify-center">
+            <div className="overflow-hidden text-center transition duration-200 ease-in transform rounded-lg hover:shadow-2xl hover:scale-105">
+              <div id="title" className="w-full py-5 border-b border-gray-800">
+                <h2 className="text-3xl font-bold text-white">Mozart</h2>
+                <h3 className="mt-2 text-xl font-normal text-indigo-500">
+                  Piano 🎹
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
