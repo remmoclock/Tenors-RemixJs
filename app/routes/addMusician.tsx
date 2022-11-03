40a8b6988e130cf87c32f562198051b445832174
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-11-03 12:34:57 +0100

    add condition for instruments

--- app/routes/addMusician.tsx
+++ app/routes/addMusician.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "@remix-run/react";
 import Button from "../Components/Button";
-import Card from "../Components/Card";
 
 function addMusician() {
   return (
@@ -20,6 +19,39 @@ function addMusician() {
             <Link to="/">
               <Button title="Accueil" />
             </Link>
+          </div>
+          <div>
+            <p>Add your own hilarious joke</p>
+            <form method="post">
+              <div>
+                <label>
+                  Name: <input type="text" name="name" />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Content: <textarea name="content" />
+                </label>
+              </div>
+              <div>
+                <select
+                  id="instrument"
+                  name="instrument"
+                  autocomplete="instrument"
+                  className="px-3 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                >
+                  <option>Piano</option>
+                  <option>Guitare</option>
+                  <option>Trompette</option>
+                  <option>Autre</option>
+                </select>
+              </div>
+              <div>
+                <button type="submit" className="button">
+                  Add
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
